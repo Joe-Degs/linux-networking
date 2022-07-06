51a89dbdb33f3b0d8498004d8c3631cabb48c24a
Joe-Degs <jpedegs8990@gmail.com>
2022-07-06 11:33:14 +0000

    cleaning code

--- lab-setup/scripts/clusterfuck.go
+++ lab-setup/scripts/clusterfuck.go
@@ -8,14 +8,19 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"io"
+	"io/fs"
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"sync"
+
+	"github.comn/mikkeloscar/sshconfig"
 )
 
 // node_type represents the role of node a node in the cluster or
@@ -40,6 +45,7 @@ type machine struct {
 	name    string
 	kind    node_type
 	streams *std_stream
+	host    *sshconfig.SSHHost
 }
 
 type std_stream struct {
@@ -48,9 +54,52 @@ type std_stream struct {
 	in, out, err io.ReadWriteCloser
 }
 
+func split_path(path string) []string {
+	return strings.Split(path, string(filepath.Separator))
+}
+
+// this mkdirp is very wrong and might only work in this settin
+func mkdirp(path string, perm os.FileMode) error {
+	dirs := split_path(path)
+	d := split_path(out_dir)
+	name := filepath.Base(out_dir)
+	var idx int
+	for i := len(d) - 1; i >= 0; i-- {
+		if d[i] == name {
+			idx = i
+			break
+		}
+	}
+	cpath := out_dir
+	for _, s := range dirs[idx:] {
+		if s != name {
+			cpath = filepath.Join(cpath, s)
+		}
+		if err := os.Mkdir(cpath, perm); err != nil {
+			if errors.Is(err, fs.ErrExist) {
+				continue
+			}
+			return err
+		}
+	}
+	return nil
+}
+
 // open files to be used as stdin, stout, stderr for virtual machine
 func new_file_stream(dir, file string) *os.File {
-	f, err := os.OpenFile(dir+file, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
+	ndir, _ := filepath.Split(file)
+	ndir = filepath.Join(dir, ndir)
+	_, err := os.Stat(ndir)
+	if err != nil {
+		if !errors.Is(err, fs.ErrNotExist) {
+			log.Fatal(err)
+		}
+
+		if err := mkdirp(ndir, 0644); err != nil {
+			log.Fatal(err)
+		}
+	}
+	f, err := os.OpenFile(filepath.Join(dir, file), os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -61,18 +110,28 @@ func new_file_stream(dir, file string) *os.File {
 // contents of the file. this is useful for reading or writing a file
 // after its operations are done
 func open_stream(dir, file string) *os.File {
-	f, err := os.OpenFile(dir+file, os.O_RDONLY, 0644)
+	f, err := os.OpenFile(filepath.Join(dir, file), os.O_RDONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
 	return f
 }
 
-// open a  stream on the host machine connecting its remote streams to it
+// paths returns a function with a parent path that you can add multiple other
+// children files to.
+func paths(parent string) func(string) string {
+	return func(file string) string {
+		return filepath.Join(parent, file)
+	}
+}
+
+// open file streams on the host machine connecting the standard streams of the
+// remote machine to the files
 func (s *std_stream) get_new_streams(hostname string) {
-	s.in = new_file_stream(shared_directory+"\\outputs\\", hostname+"\\stdin")
-	s.out = new_file_stream(shared_directory+"\\outputs\\", hostname+"\\stdout")
-	s.err = new_file_stream(shared_directory+"\\outputs\\", hostname+"\\stderr")
+	file := paths(hostname)
+	s.in = new_file_stream(out_dir, file("stdin"))
+	s.out = new_file_stream(out_dir, file("stdout"))
+	s.err = new_file_stream(out_dir, file("stderr"))
 }
 
 // return a new stream to connect to remote host's std streams
@@ -97,10 +156,10 @@ func (s *std_stream) StreamOut() {
 	s.connected = true
 }
 
-// connect machine's stdout file to the terminal stdout
+// connect machine's stdin file to the terminal stdin
 func (s *std_stream) WriteIn() {}
 
-// connect machine's stdout file to the terminal stdout
+// connect machine's stderr file to the terminal stderr
 func (s *std_stream) StreamErr() {}
 
 func (m machine) String() string {
@@ -116,27 +175,9 @@ func mach(hostname string, hosttype node_type) machine {
 	}
 }
 
-var (
-	wg sync.WaitGroup
-
-	// shared directory of the vagrant working directory
-	shared_directory = "C:\\Users\\big yeti\\linux-networking\\lab-setup"
-
-	// command line argument variables
-	debug       bool
-	allnodes    bool
-	masters     bool
-	workers     bool
-	vm          string
-	single      string
-	script_name string
-	cmd         string
-	connect     string
-	list        string
-
-	// cluster contains all vagrant virtual machines in the cluster
-	cluster []machine
-)
+func machine_from_sshconfig(host *sshconfig.SSHHost) machine {
+	return machine{}
+}
 
 func makeCmd(command string, streams *std_stream, args ...string) *exec.Cmd {
 	cmd := exec.Command(command, args...)
@@ -223,7 +264,7 @@ func getOp(op string) Op {
 	return NOP
 }
 
-// part of the script in the virtual machine
+// path of the script in the virtual machine
 func script_path(name string) string {
 	return "/vagrant/" + name
 }
@@ -282,7 +323,7 @@ func find_machine(name string) machine {
 // this whole reading the file things does not work. probably stop
 // doing it
 func printStdout(m machine) {
-	stdout := open_stream(shared_directory+"\\outputs\\", m.name+"\\stdout")
+	stdout := open_stream(out_dir, filepath.Join(m.name, "stdout"))
 	io.Copy(os.Stdout, stdout)
 }
 
@@ -297,19 +338,87 @@ func read_std_streams(machs string) {
 	}
 }
 
+func get_ssh_config() string {
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return filepath.Join(homedir, ".ssh", "config")
+}
+
+var (
+	wg sync.WaitGroup
+
+	// shared directory of the vagrant working directory
+	//shared_directory = "C:\\Users\\big yeti\\linux-networking\\lab-setup"
+	shared_dir string
+
+	// where to keep all the file streams connected to the remote machine's
+	// standard streams
+	out_dir string
+
+	// ssh config
+	ssh_config string
+
+	// command line argument variables
+	debug       bool
+	allnodes    bool
+	masters     bool
+	workers     bool
+	vm          string
+	single      string
+	script_name string
+	cmd         string
+	connect     string
+	list        string
+
+	// cluster contains all vagrant virtual machines in the cluster
+	cluster []machine
+)
+
 func main() {
 	flag.BoolVar(&debug, "v", false, "verbose output")
 	flag.BoolVar(&allnodes, "all", false, "perform operation on all nodes in cluster")
-	flag.BoolVar(&workers, "workers", false, "perform operation on nodes designated masters in cluster")
-	flag.BoolVar(&masters, "masters", false, "perform operation on nodes designated workers in cluster")
+	flag.BoolVar(&workers, "workers", false, "perform operation on nodes designated workers in cluster")
+	flag.BoolVar(&masters, "masters", false, "perform operation on nodes designated masters in cluster")
 	flag.StringVar(&vm, "vm", "", "specify vboxmanage command for vms in cluster")
 	flag.StringVar(&single, "single", "", "specify a single remote host on which to run script or command")
 	flag.StringVar(&script_name, "script", "", "specify path of script to run on remote host's shell\nthe script must be in the shared vagrant directory and relative to it")
 	flag.StringVar(&cmd, "cmd", "", "specify a command to run on the remote host's shell")
 	flag.StringVar(&connect, "c", "", "connect a machines stderr and stdout to terminal")
 	flag.StringVar(&list, "l", "", "specify comma separated list of nodes to read stdout and stderr to terminal")
+	flag.StringVar(&shared_dir, "s", "", "specify the path of the vagrant directory")
+	flag.StringVar(&out_dir, "o", "", "specify path of output streams of remote host machines")
+	flag.StringVar(&ssh_config, "ssh_config", "", "specify path to openssh config file")
 
 	flag.Parse()
+
+	var err error
+	if shared_dir == "" {
+		flag.Usage()
+		os.Exit(1)
+	} else {
+		if !filepath.IsAbs(shared_dir) {
+			if shared_dir, err = filepath.Abs(shared_dir); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	if out_dir == "" {
+		out_dir = filepath.Join(shared_dir, "outputs")
+	} else {
+		if !filepath.IsAbs(out_dir) {
+			if out_dir, err = filepath.Abs(out_dir); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	if ssh_config == "" {
+		ssh_config = get_ssh_config()
+	}
+	log.Fatal(ssh_config)
 
 	// read stdout and stdout of machines
 	if list != "" {
@@ -317,12 +426,12 @@ func main() {
 		return
 	}
 
-	cluster = append(cluster, []machine{
-		mach("master0", MASTER), mach("master1", MASTER), mach("master2", MASTER),
-		mach("worker0", WORKER), mach("worker1", WORKER), mach("worker2", WORKER),
-		mach("worker3", WORKER), mach("worker4", WORKER), mach("worker5", WORKER),
-		mach("gateway", GATEWAY),
-	}...)
+	// cluster = append(cluster, []machine{
+	// 	mach("master0", MASTER), mach("master1", MASTER), mach("master2", MASTER),
+	// 	mach("worker0", WORKER), mach("worker1", WORKER), mach("worker2", WORKER),
+	// 	mach("worker3", WORKER), mach("worker4", WORKER), mach("worker5", WORKER),
+	// 	mach("gateway", GATEWAY),
+	// }...)
 
 	// run vboxmanage operation
 	if vm != "" {
